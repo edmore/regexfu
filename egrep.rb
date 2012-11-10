6b9eaebd876aa5b09e0f32c36da9dad4e9ef6906
Edmore Moyo <etmoyo@Moyo-Mac.(none)>
2012-11-10 09:37:20 +0200

    Refactoring script to have better output

--- egrep.rb
+++ egrep.rb
@@ -1,16 +1,21 @@
 #! /usr/bin/env ruby
 
-def evaluate( expression )
+def evaluate( expression, description )
+  puts description
+  puts "[ Regex:  " + expression + " ] "
   system "egrep '#{expression}' supadupafile.txt"
 end
 
-# Test character classes
-evaluate '[Gg]r[ae]y'
-# Extracting some HTML using range within character class
-evaluate '</?H[1-6]>'
-# Character classes represent one character
-evaluate '^[a-z0-9]@[a-z].[a-z]'
-# Dot matches any character; can be too forgiving at times
-evaluate '01.12.82'
-# Alternatives
-evaluate '^(From|Subject):'
+evaluate '[Gg]r[ae]y', '# Test character classes'
+
+evaluate '</?H[1-6]>', '# Extracting some HTML using range within character class'
+
+evaluate '^[a-z0-9]@[a-z].[a-z]', '# Character classes represent one character'
+
+evaluate '01.12.82', '# Dot matches any character; can be too forgiving at times'
+
+evaluate '^(From|Subject):', '# Alternatives'
+
+evaluate '(Fir|1)st [Ss]treet', '# More alternatives'
+
+evaluate '(First|1st) (S|s)treet', '# More readable maybe?'
